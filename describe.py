85a3630f96f3094f0bb71f5a5de35cee74ecbdb9
Guy SHARONY <gsharony@bess-f3r5s5.clusters.42paris.fr>
2024-02-05 14:00:55 +0100

    Displaying column values

--- describe.py
+++ describe.py
@@ -1,59 +1,19 @@
-class Statistician:
+import sys
+import pandas as pd
 
-    @staticmethod
-    def mean(x) -> Union[float, None]:
-        if len(x) == 0:
-            return None
+def main():
+    try:
+        filename = sys.argv[1]
+        dataset = pd.read_csv(filename, index_col=0)
 
-        return sum(x) / len(x)
+        columns = dataset.select_dtypes(include="number").columns.tolist()
 
-    @staticmethod
-    def median(x) -> Union[float, None]:
-        return TinyStatistician.percentile(x, 50)
+        for column in columns:
+            values = dataset[column].tolist()
+            print(values)
 
-    @staticmethod
-    def quartile(x) -> Union[float, None]:
-        if len(x) == 0:
-            return None
-
-        return [
-            TinyStatistician.percentile(x, 25),
-            TinyStatistician.percentile(x, 75)
-        ]
-
-    @staticmethod
-    def percentile(x, p) -> Union[float, None]:
-        if not x:
-            return None
-
-        if not 0 <= p <= 100:
-            return None
-
-        data_sorted = sorted(x)
-        index = (p / 100) * (len(data_sorted) - 1)
-        lower_index = int(index // 1)
-        upper_index = lower_index + 1
-        lower_value = data_sorted[lower_index]
-        upper_value = data_sorted[upper_index]
-
-        return (1 - (index - lower_index)) * lower_value + (index - lower_index) * upper_value
-
-    @staticmethod
-    def var(x) -> Union[float, None]:
-        if len(x) == 0:
-            return None
-
-        mean = TinyStatistician.mean(x)
-        return sum([(i - mean) ** 2 for i in x]) / (len(x) - 1)
-
-    @staticmethod
-    def std(x) -> Union[float, None]:
-        variance = TinyStatistician.var(x)
-
-        if variance is None:
-            return None
-
-        return variance ** 0.5
+    except Exception as err:
+        print(f'error: {err}')
 
 if __name__ == "__main__":
-    
+    main()

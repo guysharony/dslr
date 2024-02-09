8fd0aa21936a7e3c82dfb8fc9e3f8df629dbf3de
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2024-02-09 10:03:52 +0100

    Fixed prediction

--- logreg_train.py
+++ logreg_train.py
@@ -5,6 +5,8 @@ from src.DataProcess import data_process
 from src.LogisticRegression import LogisticRegression
 from src.file_management import save_parameters_to_file
 
+house_names = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+
 if __name__ == "__main__":
     try:
         assert len(sys.argv) == 2, "1 argument required"
@@ -12,9 +14,13 @@ if __name__ == "__main__":
         dataset = pd.read_csv(sys.argv[1])
         x, y = data_process(dataset, 'train model')
 
+        # model
         model = LogisticRegression()
+
+        # Training
         weights, bias = model.fit(x, y)
 
+        # Saving thetas
         save_parameters_to_file({
             'weights': weights,
             'bias': bias
--- src/DataProcess.py
+++ src/DataProcess.py
@@ -1,5 +1,6 @@
 import time
 import sys as sys
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 

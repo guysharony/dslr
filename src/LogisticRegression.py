a02227cdefa033162941e26b7322c14c0569e081
Jee <jeehyun24park@gmail.com>
2024-02-21 12:41:19 +0100

    added parameters checking at class init

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -16,12 +16,19 @@ class LogisticRegression:
             bias (int): initial bias for the model. Default is 0.
             batch_size (int): size of the batch for gradient descent. Defaults to None.
         """
+        assert learning_rate is None or (type(learning_rate) in [float, int]) or learning_rate > 0, 'Learning rate must be positive or None'
+        assert max_iterations is None or (type(max_iterations) == int) and max_iterations > 0, "Number of maximum iterations must be a positive integer or None"
+        assert all(isinstance(weight, float) for weight in weights), 'Weights must be a list of floats'
+        assert isinstance(bias, (float, list)) and (isinstance(bias, float) or all(isinstance(b, float) for b in bias)), "Bias must be a float or a list of floats"
+        assert multi_class == 'ovr' or multi_class == 'multinomial', "Invalid classification method. Choose between <ovr> or <multinomial>"
+        assert batch_size is None or (type(batch_size) == int) or batch_size > 0, "Batch size must be positive or None."
+        
         self.learning_rate = learning_rate
         self.max_iterations = max_iterations
         self.weights = weights
         self.bias = bias
-        self.batch_size = batch_size
         self.multi_class = multi_class
+        self.batch_size = batch_size
 
     def one_hot_encode(self, y) -> np.array:
         """
@@ -37,6 +44,35 @@ class LogisticRegression:
         for i in range(len(y)):
             one_hot_y[i, y[i]] = 1
         return one_hot_y
+    
+    def create_batches(self, x, y, i):
+        if self.batch_size is None: # Batch gradient descent
+            x_batch = x
+            y_batch = y
+        elif self.batch_size == 1: # Stochastic gradient descent
+            random_index = np.random.randint(0, x.shape[0])
+            x_batch = x[random_index].reshape(1, -1)
+            y_batch = y[random_index].reshape(1, -1)
+        else: # Mini-batch gradient descent
+            np.random.seed(i)
+
+            indices = np.arange(x.shape[0])
+            np.random.shuffle(indices)
+
+            x_shuffled = x[indices]
+            y_shuffled = y[indices]
+
+            x_batch = x_shuffled[:self.batch_size]
+            y_batch = y_shuffled[:self.batch_size]
+
+        return x_batch, y_batch
+
+    def activation(self, z):
+        if self.multi_class == 'ovr':
+            return self.sigmoid(z)
+        elif self.multi_class == 'multinomial':
+            return self.softmax(z)
+        raise ValueError('invalid method')
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
@@ -56,24 +92,31 @@ class LogisticRegression:
         exp_z = np.exp(z)
         return exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
-    def cross_entropy_loss(self, y_true, y_pred) -> np.array:
+    def cross_entropy_loss(self, y_true, y_predictions) -> np.array:
         """
         Compute the binary or multiclass cross-entropy loss 
 
         Args:
             y_true (numpy.ndarray): Array of true labels in one-hot encoded format.
-            y_pred (numpy.ndarray): Array of predicted probabilities for each class.
+            y_predictions (numpy.ndarray): Array of predicted probabilities for each class.
 
         Returns:
             float: cross-entropy loss.
         """
         m       = y_true.shape[0]
         epsilon = 1e-15
-        y_pred  += epsilon
+        y_predictions  += epsilon
 
         if self.multi_class == 'ovr':
-            return -(1/m) * np.sum((y_true * np.log(y_pred)) + (1 - y_true) * np.log(1 - y_pred))
-        return -(1/m) * np.sum(y_true * np.log(y_pred))
+            return -(1/m) * np.sum((y_true * np.log(y_predictions)) + (1 - y_true) * np.log(1 - y_predictions))
+        return -(1/m) * np.sum(y_true * np.log(y_predictions))
+    
+    def plot_loss(self, costs):
+        print(f'Cost : [{costs[0]}] -> [{costs[-1]}]')
+
+        plt.plot(range(1, self.max_iterations + 1), costs)
+        plt.xlabel('Iterations')
+        plt.ylabel('Cost')
 
     def fit(self, x:np.ndarray, y:np.ndarray):
         """
@@ -91,12 +134,12 @@ class LogisticRegression:
             np.ndarray: Tuple containing the trained weights and bias
         """
         if self.multi_class == 'ovr':
-            return self._fit_ovr(x, y)
+            return self.fit_ovr(x, y)
         elif self.multi_class == 'multinomial':
-            return self._fit_multinomial(x, y)
+            return self.fit_multinomial(x, y)
         raise ValueError("Invalid method.")
     
-    def _fit_ovr(self, x:np.ndarray, y:np.ndarray):
+    def fit_ovr(self, x:np.ndarray, y:np.ndarray):
         """
         Fit the logistic regression model using the one-vs-rest method.
 
@@ -116,103 +159,63 @@ class LogisticRegression:
 
         for class_label in classes:
             y_binary = (y == class_label).astype(int)
-            weights, bias = self._gradient_descent(x, y_binary)
+            weights, bias = self.gradient_descent(x, y_binary)
             self.weights.append(weights)
             self.bias.append(bias)
 
         return self.weights, self.bias
 
-    def _gradient_descent(self, x, y):
-        m, n = x.shape
-        weights = np.zeros(n)
+    def gradient_descent(self, x, y):
+        weights = np.zeros(x.shape[1])
         bias = 0
 
         costs = []
         for i in range(self.max_iterations):
-            if self.batch_size is None: # Batch gradient descent
-                x_batch = x
-                y_batch = y
-            elif self.batch_size == 1: # Stochastic gradient descent
-                random_index = np.random.randint(0, m)
-                x_batch = x[random_index].reshape(1, -1)
-                y_batch = y[random_index].reshape(1, -1)
-            else: # Mini-batch gradient descent
-                np.random.seed(i)
+            x_batch, y_batch = self.create_batches(x, y, i)
 
-                indices = np.arange(m)
-                np.random.shuffle(indices)
+            # Prediction
+            y_predictions = self.activation(np.dot(x_batch, weights) + bias)
 
-                x_shuffled = x[indices]
-                y_shuffled = y[indices]
+            # Computing derivative
+            dw = (1 / len(x_batch)) * np.dot(x_batch.T, (y_predictions - y_batch.reshape(-1)))
+            db = (1 / len(x_batch)) * np.sum(y_predictions - y_batch)
 
-                x_batch = x_shuffled[:self.batch_size]
-                y_batch = y_shuffled[:self.batch_size]
-            
-            z = np.dot(x_batch, weights) + bias
-            y_pred = self.sigmoid(z)
-
-            # dw = (1 / len(x_batch)) * np.dot(x_batch.T, (y_pred - y_batch))
-            dw = (1 / len(x_batch)) * np.dot(x_batch.T, (y_pred - y_batch.reshape(-1)))
-            db = (1 / len(x_batch)) * np.sum(y_pred - y_batch)
-
+            # Updating weights and bias
             weights -= self.learning_rate * dw
             bias -= self.learning_rate * db
 
             # Saving cost
-            cost = self.cross_entropy_loss(y_batch, y_pred)
+            cost = self.cross_entropy_loss(y_batch, y_predictions)
             costs.append(cost)
 
-        print(f'Cost : [{costs[0]}] -> [{costs[-1]}]')
-
-        plt.plot(range(1, self.max_iterations + 1), costs)
-        plt.xlabel('Iterations')
-        plt.ylabel('Cost')
+        self.plot_loss(costs)
         
         return weights, bias
 
     
-    def _fit_multinomial(self, x:np.ndarray, y:np.ndarray):
-        m, n = x.shape
+    def fit_multinomial(self, x:np.ndarray, y:np.ndarray):
         y_encoded = self.one_hot_encode(y)
+        print(y_encoded.shape)
+        self.weights = np.zeros((x.shape[1], y_encoded.shape[1]))
 
-        self.weights = np.zeros((n, y_encoded.shape[1]))
         self.bias = 0
 
         costs = []
         for i in range(self.max_iterations):
-            if self.batch_size is None: # Batch gradient descent
-                x_batch = x
-                y_batch = y_encoded
-            elif self.batch_size == 1: # Stochastic gradient descent
-                random_index = np.random.randint(0, m)
-
-                x_batch = x[random_index].reshape(1, -1)
-                y_batch = y_encoded[random_index].reshape(1, -1)
-            else: # Mini-batch gradient descent
-                np.random.seed(i)
-                
-                indices = np.arange(m)
-                np.random.shuffle(indices)
-
-                x_shuffled = x[indices]
-                y_shuffled = y_encoded[indices]
-
-                x_batch = x_shuffled[:self.batch_size]
-                y_batch = y_shuffled[:self.batch_size]
+            x_batch, y_batch = self.create_batches(x, y_encoded, i)
 
             # Prediction
-            y_prediction = self.softmax(x_batch)
-
+            y_predictions = self.activation(x_batch)
             # Computing derivative
-            dw = (1 / len(x_batch)) * np.dot(x_batch.T, (y_prediction - y_batch))
-            db = (1 / len(x_batch)) * np.sum(y_prediction - y_batch, axis=0)
+            dw = (1 / len(x_batch)) * np.dot(x_batch.T, (y_predictions - y_batch))
+            db = (1 / len(x_batch)) * np.sum(y_predictions - y_batch)
 
             # Updating weights and bias
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
 
             # Saving cost
-            cost = self.cross_entropy_loss(y_batch, y_prediction)
+            cost = self.cross_entropy_loss(y_batch, y_predictions)
             costs.append(cost)
 
         print(f'Cost : [{costs[0]}] -> [{costs[-1]}]')
@@ -221,6 +224,8 @@ class LogisticRegression:
         plt.xlabel('Iterations')
         plt.ylabel('Cost')
 
+        # self.weights = weights
+        # self.bias = bias 
         return self.weights, self.bias
 
     def predict(self, x):
@@ -255,5 +260,5 @@ class LogisticRegression:
     def _predict_multinomial(self, x):
         if np.array(len(self.weights) == 0 or self.bias == 0).any():
             raise ValueError("Weights or bias not initialized. Model must be trained before making predictions.")
-        y_pred = self.softmax(x)
-        return np.argmax(y_pred, axis=1)
+        y_predictions = self.softmax(x)
+        return np.argmax(y_predictions, axis=1)
